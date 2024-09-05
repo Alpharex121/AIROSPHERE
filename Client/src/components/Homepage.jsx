d364be4fb3e7cdcc7fa731202fbe9e0eee2e2307
Anmol <anmolawasthi117@gmail.com>
2024-09-06 00:50:35 +0530

    hero sec added

--- Client/src/components/Homepage.jsx
+++ Client/src/components/Homepage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Spline from '@splinetool/react-spline';
 
 const Homepage = () => {
-  return <div className="h-[82vh]">Homepage</div>;
+  return <div className="h-[82vh]">
+     <Spline scene="https://prod.spline.design/nqM8c2080JyOKqWW/scene.splinecode" />
+
+  </div>;
 };
+
+
+
+
+
 
 export default Homepage;
